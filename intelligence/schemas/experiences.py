46cb796ffc17df46872fc84b388c96a62bf25d94
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-04 03:03:14 +0530

    CREATE: schemas for contextualizers
    Used to communicate with database during contextualization.

---
+++ intelligence/schemas/experiences.py
@@ -0,0 +1,15 @@
+# Schemas for experiences
+
+
+def experience_individual_serial(experience) -> dict:
+    return {
+        "id": str(experience["_id"]),
+        "name": experience.get("name"),
+        "description": experience.get("description"),
+        "cost": experience.get("cost"),
+        "location": experience.get("location"),
+    }
+
+
+def experience_list_serial(experiences) -> list:
+    return (experience_individual_serial(experience) for experience in experiences)
