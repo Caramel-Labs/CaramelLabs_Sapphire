4a0ac361e60efc4b5cac9152d166c9dc37498a96
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-04 03:18:14 +0530

    CREATE: face recognition module

---
+++ intelligence/biometrics/face.py
@@ -0,0 +1,14 @@
+from deepface import DeepFace
+
+
+class FaceExaminer:
+    def __init__(self, model="VGG-Face") -> None:
+        self.model_name = model
+
+    def check_faces(self, photo_1_link, photo_2_link):
+        result = DeepFace.verify(
+            img1_path=photo_1_link,
+            img2_path=photo_2_link,
+            model_name=self.model_name,
+        )
+        return result
