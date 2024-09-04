adc94648a31fddf2de3cba223a45e7c24555a441
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-04 21:08:41 +0530

    CREATE: routers for VisaGuard and chabot

---
+++ intelligence/routers/chatbot.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from models.chatbot import ChatDataModel
+from database.config import remote_mongodb
+from reasoning.agents import ToolBoundAgentBuilder
+
+# Setup chatbot router
+router = APIRouter(prefix="/chatbot")
+
+# Setup database connection
+db = remote_mongodb()
+
+
+# --------------------------------
+#             ROUTES
+# --------------------------------
+
+
+# Test router health
+@router.get("/ping")
+def test_router():
+    return {
+        "message": "Chatbot router is up and running.",
+    }
+
+
+@router.post("/get-response/")
+def get_response(input: ChatDataModel):
+    message = input.content
+    agent = ToolBoundAgentBuilder()
+    response = agent.invoke({"input": message})
+    return {
+        "data": response,
+    }
