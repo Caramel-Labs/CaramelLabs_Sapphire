21e3ada334d381375f48fc0419a8c18d486b9067
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-05 13:13:21 +0530

    CREATE: messages schema

---
+++ intelligence/schemas/messages.py
@@ -0,0 +1,14 @@
+# Schemas for messages
+
+
+def message_individual_serial(message) -> dict:
+    return {
+        "id": str(message["_id"]),
+        "isbot": message.get("isbot"),
+        "content": message.get("content"),
+        "user": message.get("user"),
+    }
+
+
+def message_list_serial(messages) -> list:
+    return [message_individual_serial(message) for message in messages]
