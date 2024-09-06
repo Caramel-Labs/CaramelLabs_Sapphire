193402d9d990854474430ed73a9d3e5817cee56c
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-06 13:28:57 +0530

    FIX: navbar menu text color

--- client/admin/src/components/navItem.jsx
+++ client/admin/src/components/navItem.jsx
@@ -4,7 +4,7 @@ export default function NavItem({ text, route }) {
     return (
         <li>
             <Link href={route}>
-                <div className="flex items-center p-2 hover:bg-gray-100 rounded">
+                <div className="flex text-slate-700 items-center p-2 hover:bg-gray-100 rounded">
                     <span className="mr-2"></span>
                     <span>{text}</span>
                 </div>
