9197251f438c365b30e4fad4a8f667db0c600199
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-06 14:23:39 +0530

    MINOR: login page changes

--- client/admin/src/app/page.js
+++ client/admin/src/app/page.js
@@ -1,48 +1,53 @@
+import Navbar from '@/components/navbar';
+
 export default function Login() {
     return (
-        <div className="flex h-screen bg-gray-100 items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
-                <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
-                    Log in to Sapphire Visa Console
-                </h2>
-                <form>
-                    <div className="mb-4">
-                        <label
-                            htmlFor="email"
-                            className="block text-gray-700 font-medium mb-2"
+        <div className="bg-gray-100">
+            <Navbar showUserSection={false} />
+            <div className="flex h-screen items-center justify-center">
+                <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+                    <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
+                        Log in to Sapphire Visa Console
+                    </h2>
+                    <form>
+                        <div className="mb-4">
+                            <label
+                                htmlFor="email"
+                                className="block text-gray-700 font-medium mb-2"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-900"
+                                placeholder="Email"
+                                required
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label
+                                htmlFor="password"
+                                className="block text-gray-700 font-medium mb-2"
+                            >
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-900"
+                                placeholder="Password"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition duration-200"
                         >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-900"
-                            placeholder="Email"
-                            required
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label
-                            htmlFor="password"
-                            className="block text-gray-700 font-medium mb-2"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-900"
-                            placeholder="Password"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition duration-200"
-                    >
-                        Sign In
-                    </button>
-                </form>
+                            Sign In
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
