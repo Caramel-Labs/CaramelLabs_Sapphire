9f4eb013b71722d493fbd06d5d937f768eae5453
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-06 13:40:15 +0530

    MINOR: add searchbar prop to header

--- client/admin/src/components/header.jsx
+++ client/admin/src/components/header.jsx
@@ -1,15 +1,21 @@
-export default function Header() {
-    return(
+export default function Header({ showSearch = true }) {
+    return (
         <header className="bg-white p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-semibold text-black">Sapphire Visa Console</h1>
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Search anything here..."
-            className="pl-8 pr-4 py-2 border rounded-lg"
-          />
-          <span className="absolute left-2 top-1/2 transform -translate-y-1/2">🔍</span>
-        </div>
-      </header>
-    )
+            <h1 className="text-2xl font-semibold text-black">
+                Sapphire Visa Console
+            </h1>
+            {showSearch && (
+                <div className="relative">
+                    <input
+                        type="text"
+                        placeholder="Search anything here..."
+                        className="pl-8 pr-4 py-2 border rounded-lg"
+                    />
+                    <span className="absolute left-2 top-1/2 transform -translate-y-1/2">
+                        🔍
+                    </span>
+                </div>
+            )}
+        </header>
+    );
 }
