a13b8fb5d507f1da83ef41bf465bd546b246cd4b
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-06 17:33:30 +0530

    FIX: experience generation in web frontend

--- client/mobile/app/experience/[id]/page.jsx
+++ client/mobile/app/experience/[id]/page.jsx
@@ -1,60 +1,70 @@
-'use client'
+'use client';
 
-import { useState, useEffect } from 'react'
-import BucketList from '@/app/components/bucketList'
+import { useState, useEffect } from 'react';
+import BucketList from '@/app/components/bucketList';
 
-export default function Experience() {
-  const [experienceData, setExperienceData] = useState(null);
+export default function Experience({ params }) {
+    const [experienceData, setExperienceData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:4000/api/experiences/66d42722d490bb3a58bfbe98`);
-        const result = await response.json();
-        setExperienceData(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    `http://localhost:4000/api/experiences/visited/?userId=66d1fba57c2ac1b30482e2b2&experienceId=${params.id}`
+                );
+                const result = await response.json();
+                setExperienceData(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  if (experienceData) console.log(experienceData);
+    if (experienceData) console.log(experienceData);
 
-  const { experience, hotels } = experienceData || {}
+    const { experience, updatedBucketList } = experienceData || {};
 
-  return (
-    experience && (
-      <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-        {/* Experience Image */}
-        <img
-          src='/images/headerImg.png'
-          alt={experience.name || ""}
-          className="w-full h-48 object-cover"
-        />
+    return (
+        experience && (
+            <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+                {/* Experience Image */}
+                <img
+                    src="/images/headerImg.png"
+                    alt={experience.name || ''}
+                    className="w-full h-48 object-cover"
+                />
 
-        <div className="p-4">
-          {/* Experience Name */}
-          <h2 className="text-[22px] font-semibold mb-3 mt-4 text-black">{experience.name}</h2>
+                <div className="p-4">
+                    {/* Experience Name */}
+                    <h2 className="text-[22px] font-semibold mb-3 mt-4 text-black">
+                        {experience.name}
+                    </h2>
 
-          {/* Experience Location */}
-          <p className="text-xs font-semibold text-black mb-1">{experience.oneliner || "No Oneliner"}</p>
-          <p className="text-black mb-4 text-xs">{experience.location?.address}</p>
+                    {/* Experience Location */}
+                    <p className="text-xs font-semibold text-black mb-1">
+                        {experience.oneliner || 'Sapphire Experience'}
+                    </p>
+                    <p className="text-black mb-4 text-xs">
+                        {experience.location?.address}
+                    </p>
 
-          {/* Divider Line between Location and Description */}
-          <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
+                    {/* Divider Line between Location and Description */}
+                    <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
 
-          {/* Experience Description */}
-          <p className="text-black mb-4 text-xs">{experience.description}</p>
+                    {/* Experience Description */}
+                    <p className="text-black mb-4 text-xs">
+                        {experience.description}
+                    </p>
 
-          {/* Divider Line after Description */}
-          <div className="w-full h-[1px] bg-gray-300 mb-6"></div>
+                    {/* Divider Line after Description */}
+                    <div className="w-full h-[1px] bg-gray-300 mb-6"></div>
 
-          {/* Bucket List */}
-          <BucketList bucketlistItems={experience.bucketlist} />
-        </div>
-      </div>
-    )
-  );
+                    {/* Bucket List */}
+                    <BucketList bucketlistItems={updatedBucketList} />
+                </div>
+            </div>
+        )
+    );
 }
