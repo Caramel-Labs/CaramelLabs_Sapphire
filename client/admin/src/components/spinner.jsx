6a795d1bf70de6e3da6ad4b1656ee66a27db7f93
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-06 14:39:07 +0530

    CREATE: spinner component

---
+++ client/admin/src/components/spinner.jsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+    return (
+        <div className="flex items-center justify-center h-full">
+            <div className="w-16 h-16 border-4 border-t-4 border-gray-200 rounded-full border-t-blue-500 animate-spin"></div>
+        </div>
+    );
+}
