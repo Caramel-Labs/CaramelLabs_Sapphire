bed6ce5db466799c87a91753f5e90e2fde492bae
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-09-06 14:09:28 +0530

    MINOR: full name and DOB fields updated

--- client/admin/src/components/applicant/personalDetails.jsx
+++ client/admin/src/components/applicant/personalDetails.jsx
@@ -1,4 +1,15 @@
 export default function PersonalDetails({ data }) {
+    // Function to format date into "21 Aug 2003" format
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        // Using Intl.DateTimeFormat to format the date
+        return new Intl.DateTimeFormat('en-GB', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+        }).format(date);
+    };
+
     return (
         <div className="bg-white p-6 rounded-lg shadow-md mb-4">
             <h2 className="text-xl font-bold mb-4 text-gray-500">
@@ -7,11 +18,11 @@ export default function PersonalDetails({ data }) {
             <div className="grid grid-cols-2 gap-4">
                 <div>
                     <p className="text-gray-500 font-medium"> Full Name</p>
-                    <p className="text-gray-900">{data.name}</p>
+                    <p className="text-gray-900">{data.fullname}</p>
                 </div>
                 <div>
                     <p className="text-gray-500 font-medium">Date of Birth</p>
-                    <p className="text-gray-900">{data.dob}</p>
+                    <p className="text-gray-900">{formatDate(data.dob)}</p>
                 </div>
                 <div>
                     <p className="text-gray-500 font-medium">Nationality</p>
