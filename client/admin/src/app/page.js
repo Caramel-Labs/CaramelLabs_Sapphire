1232bb788d1088d7a7d117b8f414d09951cd23fe
Manujaya <manujaya.xyz@gmail.com>
2024-09-06 19:06:32 +0530

    feat: Add DonutChart component to Analytics page
    - Import DonutChart component from '@/components/charts/donutChart'
    - Add DonutChart component to the Analytics page
    
    This commit adds the DonutChart component to the Analytics page in the admin application. The DonutChart component is imported from the '@/components/charts/donutChart' module and is used to display visa counts by country code. This enhancement improves the visualization of data on the Analytics page.

--- client/admin/src/app/page.js
+++ client/admin/src/app/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Navbar from '@/components/navbar';
-
+import { signIn } from "next-auth/react";
 
 export default function Login() {
     const [email, setEmail] = useState('');
