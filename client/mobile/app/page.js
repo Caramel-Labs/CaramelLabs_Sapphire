141407a37c6416afd1e82742dd537b2966f70401
Kalana Kaushalya <115646312+Kalana-Kaushalya@users.noreply.github.com>
2024-09-06 16:03:23 +0530

    project init

---
+++ client/mobile/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Welcome() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between  bg-gray-500">
+      <h6  className="text-white my-[390px] mx-[89px] text-[22px]">Sapphire</h6>
+
+    </main>
+  );
+}
